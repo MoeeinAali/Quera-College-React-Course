a0c8bd8873f5a835de8340cb01603e9e0df60435
Moeein Aali <moeeeinaali@gmail.com>
2024-09-08 21:14:52 +0330

    Add TS Solutions

---
+++ Assignments/4-Components and TSX/خوشامدگویی/src/App.tsx
@@ -0,0 +1,12 @@
+import Greeting from "./components/Greeting";
+
+function App() {
+  return (
+    <div>
+      <h1>Quera College</h1>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ Assignments/4-Components and TSX/خوشامدگویی/src/components/Greeting.tsx
@@ -0,0 +1,7 @@
+export default function Greeting() {
+  return (
+      <p>
+        Welcome to React College.
+      </p>
+  );
+}
